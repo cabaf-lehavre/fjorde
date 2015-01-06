d5c0e3e26814f3197848607b55df2e6f63d31dd8
Blackrush <blackrushx@gmail.com>
2015-01-06 12:45:26 +0100

    Cut the crap out

--- src/fjorde/Tile.java
+++ src/fjorde/Tile.java
@@ -1,10 +1,7 @@
 package fjorde;
 
-public class Tile
-{
+public class Tile {
 	private TileItem[] items;
-	private boolean jail;
-	private boolean pawn;
 
 	public Tile(TileItem[] items) {
 		this.items = items;
@@ -13,30 +10,4 @@ public class Tile
 	public Tile() {
 		this(TileItems.samples(6));
 	}
-
-	public void setJail(){
-		if (pawn==false){
-			jail = true;
-		}
-		else {
-			System.out.println("Deja un item !");
-		}
-	}
-
-	public void setPawn(){
-		if(jail == false){
-			pawn = true;
-		}
-		else {
-			System.out.println("Deja un item !");
-		}
-	}
-
-	public boolean getJail(){
-		return jail;
-	}
-
-	public boolean getPawn(){
-		return pawn;
-	}	
 }
