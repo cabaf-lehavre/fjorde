734f1e854a4653df4bc0d636b534504805d57541
Blackrush <blackrushx@gmail.com>
2015-01-06 17:22:44 +0100

    Create a way to retrieve a TileItem from a Tile

--- src/fjorde/Tile.java
+++ src/fjorde/Tile.java
@@ -10,4 +10,21 @@ public class Tile {
 	public Tile() {
 		this(TileItems.samples(6));
 	}
+
+	public TileItem getItem(int i) {
+		return items[i];
+	}
+
+	public TileItem getItem(Cardinal c) {
+		int i = c.ordinal();
+
+		// ignore EAST and WEST
+		if (i < 2) {
+			return getItem(i);
+		} else if (i < 6) {
+			return getItem(i - 1);
+		} else {
+			return getItem(i - 2);
+		}
+	}
 }
