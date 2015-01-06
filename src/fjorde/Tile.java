9a9ef5060f6fe4ec92ea1b53469c5eff09d19788
Blackrush <blackrushx@gmail.com>
2015-01-06 17:29:00 +0100

    A Tile has multiple neighbours (as many as he have of corner)

--- src/fjorde/Tile.java
+++ src/fjorde/Tile.java
@@ -1,14 +1,18 @@
 package fjorde;
 
 public class Tile {
-	private TileItem[] items;
+	public static final int CORNERS = 6;
 
-	public Tile(TileItem[] items) {
+	private TileItem[] items;
+	private Tile[] neighbours;
+
+	public Tile(TileItem[] items, Tile[] neighbours) {
 		this.items = items;
+		this.neighbours = neighbours;
 	}
 
 	public Tile() {
-		this(TileItems.samples(6));
+		this(TileItems.samples(CORNERS), new Tile[CORNERS]);
 	}
 
 	public TileItem getItem(int i) {
@@ -16,15 +20,38 @@ public class Tile {
 	}
 
 	public TileItem getItem(Cardinal c) {
-		int i = c.ordinal();
+		// forbid EAST and WEST
+		if (c == Cardinal.EAST || c == Cardinal.WEST) {
+			throw new IllegalArgumentException();
+		}
 
-		// ignore EAST and WEST
+		// align indices
+		int i = c.ordinal();
 		if (i < 2) {
 			return getItem(i);
 		} else if (i < 6) {
 			return getItem(i - 1);
 		} else {
 			return getItem(i - 2);
+		}
+	}
+
+	public Tile getNeighbour(int i) {
+		return neighbours[i];
+	}
+
+	public Tile getNeighbour(Cardinal c) {
+		// forbid NORTH and SOUTH
+		if (c == Cardinal.NORTH || c == Cardinal.SOUTH) {
+			throw new IllegalArgumentException();
+		}
+
+		// align indices
+		int i = c.ordinal();
+		if (i < 4) {
+			return getNeighbour(i - 1);
+		} else {
+			return getNeighbour(i - 2);
 		}
 	}
 }
