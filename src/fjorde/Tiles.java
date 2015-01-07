ace34e477863c22a713792e82173d35bc379b795
Blackrush <blackrushx@gmail.com>
2015-01-07 11:25:25 +0100

    More convenient way to create tiles

--- src/fjorde/Tiles.java
+++ src/fjorde/Tiles.java
@@ -12,6 +12,32 @@ public final class Tiles {
     private Tiles() {}
 
     /**
+     * Create a tile without neighbours
+     * @param corners a non-null array of non-null items
+     * @return a non-null tile
+     */
+    public static Tile of(TileItem... corners) {
+        if (corners.length != Tile.CORNERS) {
+            throw new IllegalArgumentException();
+        }
+        return new Tile(corners, new Tile[Tile.CORNERS]);
+    }
+
+    /**
+     * Create a tile without neighbours using {@link fjorde.TileItems#create(int)}
+     * @param indexes a non-null array of integer
+     * @return a non-null tile
+     */
+    public static Tile of(int... indexes) {
+        TileItem[] corners = new TileItem[indexes.length];
+        for (int i = 0; i < indexes.length; i++) {
+            corners[i] = TileItems.create(indexes[i]);
+        }
+
+        return of(corners);
+    }
+
+    /**
      * Randomly create a tile
      * @param random a non-null random generator
      * @return a non-null tile
