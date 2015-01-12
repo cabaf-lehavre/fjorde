356bf4024e5a5be0f83d5a53bf4543b5b040daec
Blackrush <blackrushx@gmail.com>
2015-01-12 13:30:05 +0100

    Test completeness

--- test/fjorde/TexturesTest.java
+++ test/fjorde/TexturesTest.java
@@ -5,12 +5,14 @@ import java.io.File;
 import java.util.Map;
 
 import static unit.Assert.assertEquals;
+import static unit.Assert.assertTrue;
 
 public class TexturesTest {
     public static void main(String[] args) {
         Map<String, Image> map = Textures.load(new File("img"));
 
         assertEquals(map.size(), 88, "number of textures");
+        assertTrue(map.containsKey("MMMMMM"), "has texture full of mountain");
 
         System.out.println("success");
     }
