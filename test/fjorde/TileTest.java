d78ab31cd12038bcb047ea3b176a5db6d1387fb5
Blackrush <blackrushx@gmail.com>
2015-01-14 13:09:02 +0100

    Rotate a Tile

--- test/fjorde/TileTest.java
+++ test/fjorde/TileTest.java
@@ -2,7 +2,7 @@ package fjorde;
 
 import java.util.Random;
 
-import static fjorde.TileItems.MOUNTAIN;
+import static fjorde.TileItems.*;
 import static unit.Assert.assertEquals;
 
 public class TileTest {
@@ -12,6 +12,19 @@ public class TileTest {
                              MOUNTAIN, MOUNTAIN, MOUNTAIN);
 
         assertEquals(tile.getSymbol(), "MMMMMM", "wrong symbol");
+
+        tile.rotate();
+        assertEquals(tile.getSymbol(), "MMMMMM", "wrong symbol");
+
+        Tile tile2 = Tiles.of(PLAIN, PLAIN, PLAIN, PLAIN, PLAIN, SEA);
+        tile2.rotate();
+        assertEquals(tile2.getSymbol(), "SPPPPP", "wrong symbol");
+        tile2.rotate();
+        assertEquals(tile2.getSymbol(), "PSPPPP", "wrong symbol");
+
+        Tile tile3 = Tiles.of(PLAIN, PLAIN, PLAIN, PLAIN, PLAIN, SEA);
+        tile3.rotate(2);
+        assertEquals(tile3.getSymbol(), tile2.getSymbol(), "wrong symbol");
 
         Tile sample = Tiles.sample(new Random(System.nanoTime()));
 
