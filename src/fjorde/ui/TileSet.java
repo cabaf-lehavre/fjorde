0b19bba2197c3ea1dd5aa15bf9df9856f7e60ab6
Blackrush <blackrushx@gmail.com>
2015-01-14 15:44:39 +0100

    Link tiles between themselves

--- src/fjorde/ui/TileSet.java
+++ src/fjorde/ui/TileSet.java
@@ -1,6 +1,10 @@
 package fjorde.ui;
 
+import fjorde.Cardinal;
 import fjorde.Tile;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Antoine CHAUVIN INFOB1
@@ -37,6 +41,10 @@ public class TileSet {
         return tile;
     }
 
+    public Tile weakGet(int x, int y) {
+        return inBounds(x, y) ? tryGet(x, y) : null;
+    }
+
     public boolean present(int x, int y) {
         return inBounds(x, y) && tryGet(x, y) != null;
     }
@@ -44,6 +52,41 @@ public class TileSet {
     public void set(int x, int y, Tile tile) {
         tiles[x][y] = tile;
         // todo neighbours
+
+        List<Tile> neighbours = around(x, y);
+        for (int i = 0; i < neighbours.size(); i++) {
+            Tile neighbour = neighbours.get(i);
+            if (neighbour == null) {
+                continue;
+            }
+
+            int j = Cardinal.neighbourIndexOpposite(i);
+
+            // from A to B
+            tile.addNeighbour(neighbour, i);
+            // from B to A
+            neighbour.addNeighbour(tile, j);
+        }
+    }
+
+    private List<Tile> around(int x, int y) {
+        // auto-correct neighbour ordinates
+        int d = x % 2;
+
+        int neX = x - 1, neY = y + d;
+        int eX = x, eY = y + 1;
+        int seX = x + 1, seY = y + d;
+        int swX = x + 1, swY = y - 1 + d;
+        int wX = x, wY = y + 1;
+        int nwX = x - 1, nwY = y - 1 + d;
+        Tile ne = weakGet(neX, neY);
+        Tile e  = weakGet(eX, eY);
+        Tile se = weakGet(seX, seY);
+        Tile sw = weakGet(swX, swY);
+        Tile w  = weakGet(wX, wY);
+        Tile nw = weakGet(nwX, nwY);
+
+        return Arrays.asList(ne, e, se, sw, w, nw);
     }
 
     public boolean trySet(int x, int y, Tile tile) {
