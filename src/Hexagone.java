b2a74e1add757c2f146a24ef2e4ba0edcd5e3cb2
Blackrush <blackrushx@gmail.com>
2015-01-14 11:19:12 +0100

    Clean up Hexagone class

--- src/Hexagone.java
+++ src/Hexagone.java
@@ -14,39 +14,35 @@ import java.awt.event.MouseEvent;
 public class Hexagone extends JFrame {
 
     Plateau panelPlateau;
-    JPanel panelPioches;
-    JPanel panelDraw;
-    JPanel panelJailsPawn;
     JailsPawn jailsPawn;
     Draw draw;
 
     public Hexagone() {
+        panelPlateau = new Plateau();
+        panelPlateau.setBackground(Color.white);
+        panelPlateau.setLayout(null);
+        panelPlateau.addMouseListener(new MouseManager());
+
+        jailsPawn = new JailsPawn(new Player(1, "Stephen", "Noir"));
+        draw = new Draw();
+
+        JPanel panelPioches = new JPanel(new GridLayout(2, 1));
+        panelPioches.add(draw);
+        panelPioches.add(jailsPawn);
+
+        add(panelPlateau);
+        add(panelPioches, BorderLayout.EAST);
+
         setTitle("Fjorde");
         setLocation(1250, 0);
         setSize(500,500);
         setDefaultCloseOperation(3);
-        mouseManager localMouseManager = new mouseManager();
-
-        panelPlateau = new Plateau();
-        panelPlateau.setBackground(Color.white);
-        panelPlateau.setLayout(null);
-        add(panelPlateau);
-        panelPlateau.addMouseListener(localMouseManager);
-
-        panelPioches = new JPanel(new GridLayout(2, 1));
-        jailsPawn = new JailsPawn(new Player(1, "Stephen", "Noir"));
-        draw = new Draw();
-        panelPioches.add(draw);
-        panelPioches.add(jailsPawn);
-
-
-        add(panelPioches, BorderLayout.EAST);
         setVisible(true);
     }
 
-    private class mouseManager extends MouseAdapter
+    private class MouseManager extends MouseAdapter
     {
-        private mouseManager() {}
+        private MouseManager() {}
 
         public void mousePressed(MouseEvent paramMouseEvent)
         {
