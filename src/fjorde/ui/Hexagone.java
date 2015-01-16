d2024eee679eb7facc639be11c3f09b5774e79ff
Blackrush <blackrushx@gmail.com>
2015-01-16 12:17:15 +0100

    Live update Scoreboard

--- src/fjorde/ui/Hexagone.java
+++ src/fjorde/ui/Hexagone.java
@@ -81,6 +81,7 @@ public class Hexagone extends JFrame {
                     }
                 }
 
+                scoreBoard.update();
             }
         }
     }
--- src/fjorde/ui/Scoreboard.java
+++ src/fjorde/ui/Scoreboard.java
@@ -11,34 +11,42 @@ import java.awt.*;
  */
 public class Scoreboard extends JPanel{
 
+    private Game game;
+
     JLabel playerOneScore = new JLabel();
     JLabel playerTwoScore = new JLabel();
     JButton playerOne = new JButton();
     JButton playerTwo = new JButton();
 
     public Scoreboard(Game game) {
-        Player player_one = game.getFirstPlayer(),
-                player_two = game.getSecondPlayer();
+        this.game = game;
 
         setLayout(null);
-        playerOneScore.setText(player_one.getName() + ": " + player_one.getScore());
-        playerTwoScore.setText(player_two.getName() + ": " + player_two.getScore());
 
         playerOne.setBackground(Color.WHITE);
         playerOne.setIcon( new ImageIcon("img/barbare1.jpg"));
+        playerOne.setBounds(10,50,50,63);
 
         playerTwo.setBackground(Color.WHITE);
         playerTwo.setIcon( new ImageIcon("img/barbare2.png"));
-
-        playerOne.setBounds(10,50,50,63);
         playerTwo.setBounds(10,130,64,50);
+
+        update();
         playerOneScore.setBounds(80,55,100,100);
         playerTwoScore.setBounds(80,120,100,100);
+
         add(playerOne);
         add(playerTwo);
         add(playerOneScore);
         add(playerTwoScore);
     }
 
+    public void update() {
+        Player player_one = game.getFirstPlayer(),
+                player_two = game.getSecondPlayer();
+
+        playerOneScore.setText(player_one.getName() + ": " + player_one.getScore());
+        playerTwoScore.setText(player_two.getName() + ": " + player_two.getScore());
+    }
 
 }
